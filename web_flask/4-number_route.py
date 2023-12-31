43a82dbed3385e5c2f7da40fbd191c4972135bb8
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-31 19:51:32 +0100

    display N

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def displayHbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def displayText(text):
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+@app.route("/python/", strict_slashes=False, defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def displayPython(text):
+    text = text.replace('_', ' ')
+    return 'Python ' + text
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def displayN(n):
+    return f"{n} is a number"
+
+
+host = '0.0.0.0'
+port = 5000
+
+if __name__ == '__main__':
+    app.run(debug=True, host=host, port=port)
